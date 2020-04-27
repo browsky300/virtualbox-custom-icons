3db8f260d23b906827b4a16fd3fd69d96bc80749
browsky300 <32629291+browsky300@users.noreply.github.com>
2020-04-27 14:53:17 -0700

    Update script.bat

--- script.bat
+++ script.bat
@@ -3,5 +3,5 @@ title VirtualBox Custom Icons Script
 echo VirtualBox Custom Icons Script
 set /p name=Enter VM Name: 
 set /p icon=Enter name of an icon in the folder: 
-"%ProgramFiles%\Oracle\VirtualBox\VBoxManage" modifyvm %name% --iconfile %icon%
+"%ProgramFiles%\Oracle\VirtualBox\VBoxManage" modifyvm "%name%" --iconfile %icon%
 pause
